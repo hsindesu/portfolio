006c42adf6c191403fb64707da05c404ef7c10ea
Hsin-Fang (Tony) Wang <hsindesu@gmail.com>
2024-10-19 20:22:47 +0900

    bug: fixed tailwind config and outputting styles.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	/** @type {import('tailwindcss').Config} */
-	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
-		},
-		fontFamily: {
-			sans: ['Graphik', 'sans-serif'],
-			serif: ['Merriweather', 'serif'],
-		},
-		extend: {
-			spacing: {
-				'128': '32rem',
-				'144': '36rem',
-			},
-			borderRadius: {
-				'4xl': '2rem',
-			},
-			fontFamily: {
-				display: 'Oswald, ui-serif',
-			}
-		}
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  /** @type {import('tailwindcss').Config} */
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        display: "Oswald, ui-serif",
+      },
+    },
+    content: [
+      "./pages/**/*.{astro,html,js}",
+      "./components/**/*.{astro,html,js}",
+    ],
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
